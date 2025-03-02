906defe5714d2373171ddd520717e5f07cc3b0fd
Luis Eduardo Imbernón <imbernon@gmail.com>
2025-03-02 12:45:15 +0100

    #89: Added documentation for range initializer and for binary initializer. #121: Added the range initializer.

--- tests/test_initializers.rs
+++ tests/test_initializers.rs
@@ -2,15 +2,15 @@
 mod structures;
 
 use genetic_algorithms::chromosomes::Binary;
-use genetic_algorithms::initializers::{binary_random_initialization, generic_random_initialization, generic_random_initialization_without_repetitions};
+use genetic_algorithms::initializers::{binary_random_initialization, generic_random_initialization, generic_random_initialization_without_repetitions, range_random_initialization};
 use genetic_algorithms::traits::ChromosomeT;
+use genetic_algorithms::genotypes::Range as RangeGenotype;
 use crate::structures::{Gene, Chromosome};
 
 #[test]
 fn test_initializers_generic_random_initialization(){
-
     let binding =  vec![Gene{id:1}, Gene{id:2}, Gene{id:3}, Gene{id:4},
-                                   Gene{id:5}, Gene{id:6}, Gene{id:7}, Gene{id:8}];
+                        Gene{id:5}, Gene{id:6}, Gene{id:7}, Gene{id:8}];
     let alleles = binding.as_slice();
 
     let genes = generic_random_initialization::<Chromosome>(4, Some(alleles), Some(false));
@@ -19,22 +19,20 @@ fn test_initializers_generic_random_initialization(){
 
 #[test]
 fn test_initializers_generic_random_initialization_without_repetitions(){
-
     let binding =  vec![Gene{id:1}, Gene{id:2}, Gene{id:3}, Gene{id:4},
-                                   Gene{id:5}, Gene{id:6}, Gene{id:7}, Gene{id:8}];
+                        Gene{id:5}, Gene{id:6}, Gene{id:7}, Gene{id:8}];
     let alleles = binding.as_slice();
     let genes = generic_random_initialization_without_repetitions::<Chromosome>(6, Some(alleles), Some(false));
 
     //Checks that any allele is repeated
     let mut alleles_ids = Vec::new();
-    
+
     for gene in genes {
         if !alleles_ids.is_empty(){
             assert!(!alleles_ids.contains(&gene.id));
         }
         alleles_ids.push(gene.id);
     }
-
 }
 
 #[test]
@@ -44,4 +42,14 @@ fn test_binary_random_initialization(){
 
     chromosome.set_dna(genes.as_slice());
     assert_eq!(100, chromosome.phenotype().len());
+}
+
+#[test]
+fn test_range_random_initialization(){
+    let alleles = vec![RangeGenotype::new(0, vec![(0.0, 1.0)], 0.0)];
+    let genes = range_random_initialization(10, Some(&alleles), Some(false));
+    assert_eq!(genes.len(), 10);
+    for gene in genes {
+        assert!(gene.value >= 0.0 && gene.value <= 1.0);
+    }
 }
